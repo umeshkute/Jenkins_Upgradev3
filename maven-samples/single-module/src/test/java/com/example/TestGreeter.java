0e995eb5652b4f5646f1374a1136dbdc73dbe866
umeshkute <53827180+umeshkute@users.noreply.github.com>
2020-06-13 20:31:07 +0530

    Update TestGreeter.java
    Added new test method

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -31,8 +31,12 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-    @Test
+  @Test
   public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, Thanks you making me Happy";
+  
+    @Test
+  public void greetShouldIncludeGreetingMessageAgain() {
     String someone = "World, Thanks you making me Happy again";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
